fcb7ad6d7973855d2b1ba1b760740e4a3202c525
Mindaugas Minelga <minelga.m@gmail.com>
2023-11-07 19:29:57 +0200

    add Flask app

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Viso gero")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Wooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooorld!</p>"
